6d46c5cb0fbb474928426da589bdc0f3a396a12c
EsmeNtb <esmeentb@gmail.com>
2026-09-04 20:14:55 -0600

    feat: add /photo
    - PhotoList, PhotoEdit, PhotoCreate and PhotoShow

--- test-admin/src/photos.tsx
+++ test-admin/src/photos.tsx
@@ -1,11 +1,31 @@
-
 import { useMediaQuery, Theme } from "@mui/material";
-import { List, SimpleList, DataTable } from "react-admin";
+import {
+    List,
+    SimpleList,
+    ImageField,
+    DataTable,
+    Edit,
+    SimpleForm,
+    TextInput,
+    Create,
+    Show,
+    SimpleShowLayout,
+    TextField,
+    ReferenceField,
+    ReferenceInput,
+} from "react-admin";
 
+const photoFilters = [
+    <TextInput key="search" source= "q" label="Search" alwaysOn />,
+    <ReferenceInput key="album" source="albumId" label="Álbum" reference="albums" />
+];
 export const PhotoList = () => {
-    const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+    const isSmall = useMediaQuery<Theme>((theme) =>
+        theme.breakpoints.down("sm")
+    );
+
     return (
-        <List>
+        <List filters={photoFilters}>
             {isSmall ? (
                 <SimpleList
                     primaryText={(record) => record.albumId}
@@ -14,13 +34,78 @@ export const PhotoList = () => {
                 />
             ) : (
                 <DataTable>
-                    <DataTable.Col source="albumId" />
+                    <DataTable.Col source="albumId">
+                        <ReferenceField
+                            source="albumId"
+                            reference="albums"
+                        />
+                    </DataTable.Col>
+
                     <DataTable.Col source="id" />
                     <DataTable.Col source="title" />
                     <DataTable.Col source="url" />
-                    <DataTable.Col source="thumbnailUrl" />
+
+                    <DataTable.Col label="Miniatura">
+                        <ImageField
+                            source="thumbnailUrl"
+                            title="title"
+                        />
+                    </DataTable.Col>
                 </DataTable>
             )}
         </List>
     );
 };
+
+export const PhotoEdit = () => (
+    <Edit>
+        <SimpleForm warnWhenUnsavedChanges>
+            <TextInput disabled source="id" />
+
+            <ReferenceInput
+                source="albumId"
+                reference="albums"
+            />
+
+            <TextInput required source="title" />
+            <TextInput source="url" />
+            <TextInput source="thumbnailUrl" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PhotoCreate = () => (
+    <Create>
+        <SimpleForm>
+            <ReferenceInput
+                source="albumId"
+                reference="albums"
+            />
+
+            <TextInput required source="title" />
+            <TextInput source="url" />
+            <TextInput source="thumbnailUrl" />
+        </SimpleForm>
+    </Create>
+);
+
+export const PhotoShow = () => (
+    <Show>
+        <SimpleShowLayout>
+            <TextField source="id" />
+
+            <ReferenceField
+                source="albumId"
+                reference="albums"
+            />
+
+            <TextField source="title" />
+            <TextField source="url" />
+
+            <ImageField
+                source="thumbnailUrl"
+                title="title"
+            />
+        </SimpleShowLayout>
+    </Show>
+);
