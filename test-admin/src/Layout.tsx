cf4bd419008d32fa83faa49e3df14f5bac3d190b
EsmeNtb <esmeentb@gmail.com>
2026-09-04 20:16:18 -0600

    feat: add Layout.tsx
    Update the Layout, using MyAppBar for the topbar

--- test-admin/src/Layout.tsx
+++ test-admin/src/Layout.tsx
@@ -1,9 +1,33 @@
-import type { ReactNode } from "react";
-import { Layout as RALayout, CheckForApplicationUpdate } from "react-admin";
+import type { ReactNode } from 'react';
+
+import {
+    Layout as RALayout,
+    CheckForApplicationUpdate,
+    AppBar,
+    LoadingIndicator,
+} from 'react-admin';
+
+import { Identity } from './Identity';
+import { ThemeToggler } from './theme';
+
+const MyToolbar = () => (
+    <>
+        <ThemeToggler />
+        <LoadingIndicator />
+        <Identity />
+    </>
+);
+
+const MyAppBar = () => (
+    <AppBar
+        toolbar={<MyToolbar />}
+        userMenu={false}
+    />
+);
 
 export const Layout = ({ children }: { children: ReactNode }) => (
-  <RALayout>
-    {children}
-    <CheckForApplicationUpdate />
-  </RALayout>
+    <RALayout appBar={MyAppBar}>
+        {children}
+        <CheckForApplicationUpdate />
+    </RALayout>
 );
