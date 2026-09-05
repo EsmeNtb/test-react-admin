ca77c5c3f05ef6be81618175e06a459c3b60b29a
EsmeNtb <esmeentb@gmail.com>
2026-09-04 20:14:13 -0600

    feat: add post from React-admin
    Add filter, postlist, postEdit, postCreate and postShow

--- test-admin/src/posts.tsx
+++ test-admin/src/posts.tsx
@@ -1,10 +1,33 @@
-import { useMediaQuery, Theme } from "@mui/material";
-import { List, SimpleList, DataTable,} from "react-admin";
+import { useMediaQuery, Theme, } from "@mui/material";
+import {
+    List,
+    SimpleList,
+    DataTable,
+    ReferenceField,
+    EditButton,
+    Edit,
+    SimpleForm,
+    TextInput,
+    TextField,
+    ReferenceInput,
+    Create,
+    FunctionField,
+    Show,
+    SimpleShowLayout,
+} from "react-admin";
+
+const postFilters = [
+    <TextInput key="search" source= "q" label="Search" alwaysOn />,
+    <ReferenceInput key="user" source="userId" label="User" reference="users" />
+];
 
 export const PostList = () => {
-    const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
+    const isSmall = useMediaQuery<Theme>((theme) =>
+        theme.breakpoints.down("sm")
+    );
+
     return (
-        <List>
+        <List filters={postFilters}>
             {isSmall ? (
                 <SimpleList
                     primaryText={(record) => record.userId}
@@ -13,12 +36,64 @@ export const PostList = () => {
                 />
             ) : (
                 <DataTable>
-                    <DataTable.Col source="userId" />
                     <DataTable.Col source="id" />
+
+                    <DataTable.Col source="userId">
+                        <ReferenceField
+                            source="userId"
+                            reference="users"
+                            link="show"
+                        />
+                    </DataTable.Col>
+
                     <DataTable.Col source="title" />
-                    <DataTable.Col source="body"/>
+
+                    {/* <DataTable.Col source="body" /> */}
+                    <DataTable.Col label="Excerpt">
+                        <FunctionField 
+                            render= {(record)  =>`${record.body.substring(0,50)}...`}/>
+                    </DataTable.Col>
+                    <DataTable.Col>
+                        <EditButton />
+                    </DataTable.Col>
                 </DataTable>
             )}
         </List>
     );
 };
+
+export const PostEdit = () => (
+    <Edit>
+        <SimpleForm warnWhenUnsavedChanges>
+            <TextInput disabled source="id" />
+
+            <ReferenceInput source="userId" reference="users"/>
+
+            <TextInput required source="title" />
+
+            <TextInput source="body" multiline rows = {5} />
+        </SimpleForm>
+    </Edit>
+);
+
+export const PostCreate = () => (
+    <Create>
+        <SimpleForm >
+            <TextInput disabled source="id"/>
+            <ReferenceInput required source="userId" reference="users"/>
+            <TextInput required source="title"/>
+            <TextInput source="body" multiline rows={5}/>
+        </SimpleForm>
+    </Create>
+);
+
+export const PostShow=() => (
+    <Show>
+        <SimpleShowLayout>
+            <ReferenceField source= "userId" reference="users"/>
+            <TextField source="id"/>
+            <TextField source="title"/>
+            <TextField source="body"/>
+        </SimpleShowLayout>
+    </Show>
+)
