ecf2fa5effbb929776e38b9c6d85c63a8488c27f
EsmeNtb <esmeentb@gmail.com>
2026-09-04 20:18:28 -0600

    feaT: add /albums.tsx
    - Albumfilters, AlbumList, AlbumEdit, AlbumCreate and AlbumShow

--- test-admin/src/albums.tsx
+++ test-admin/src/albums.tsx
@@ -1,11 +1,16 @@
 
-import { useMediaQuery, Theme } from "@mui/material";
-import { List, SimpleList, DataTable} from "react-admin";
+import { useMediaQuery, Theme, } from "@mui/material";
+import { List, SimpleList, ReferenceField,TextField ,DataTable,Edit,SimpleForm,TextInput,Create,Show,SimpleShowLayout,ReferenceInput} from "react-admin";
+
+const albumFilters = [
+    <TextInput key="search" source= "q" label="Search" alwaysOn />,
+    <ReferenceInput key="user" source="userId" label="User" reference="users" />
+];
 
 export const AlbumList = () => {
     const isSmall = useMediaQuery<Theme>((theme) => theme.breakpoints.down("sm"));
     return (
-        <List>
+        <List filters={albumFilters}>
             {isSmall ? (
                 <SimpleList
                     primaryText={(record) => record.userId}
@@ -14,11 +19,43 @@ export const AlbumList = () => {
                 />
             ) : (
                 <DataTable>
-                    <DataTable.Col source="userId" />
-                    <DataTable.Col source="id" />
-                    <DataTable.Col source="title" />
+                    <DataTable.Col source="userId">
+                    <ReferenceField source="userId" reference="users" />
+                </DataTable.Col>
+
+                <DataTable.Col source="id" />
+                <DataTable.Col source="title" />
                 </DataTable>
             )}
         </List>
     );
 };
+
+export const AlbumEdit = () => (
+    <Edit>
+        <SimpleForm warnWhenUnsavedChanges>
+            <TextInput disabled source="id" />
+            <ReferenceInput source="userId" reference="users"/>
+            <TextInput required source="title" />
+        </SimpleForm>
+    </Edit>
+);
+
+export const AlbumCreate = () => (
+    <Create>
+        <SimpleForm >
+            <ReferenceInput source="userId" reference="users"/>
+            <TextInput required source="title" />
+        </SimpleForm>
+    </Create>
+);
+
+export const AlbumShow=() => (
+    <Show>
+        <SimpleShowLayout>
+            <TextField source="id" />
+            <ReferenceField source="userId" reference="users"/>
+            <TextField source="title" />
+        </SimpleShowLayout>
+    </Show>
+);
